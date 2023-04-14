cd33258ad6383a6495d9e81ea5ea00bb97bad431
favdimples <rahel.elena.ackeret@gmail.com>
2023-04-14 17:37:22 +0200

    nothing important

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ loadSprite("green", "assets/green.png");
 loadSprite("pink", "assets/pink.png");
 loadSprite("purple", "assets/purple.png");
 loadSprite("red", "assets/red.png");
+loadSprite("long", "assets/green-long.png");
+loadSprite("wide", "assets/blue-wide.png");
 
 loadSprite("ground", "assets/ground.jpg");
 loadSprite("ceiling", "assets/ceiling.jpg");
@@ -24,14 +26,16 @@ let block = 0
 const STAY_SPEED = 0
 const TIME_LEFT = 100
 const BLOCK_SIZE = 20
-const TIME_END = 5
-const sprites = ["red", "blue", "yellow", "green", "pink", "purple"];
+const TIME_END = 8
+const TIME_INTERVAL = 1
+const sprites = ["wide", "long"];
+// const sprites = ["red", "blue", "yellow", "green", "pink", "purple"];
 const spriteName = choose(sprites)
 const spriteColor = color (rand(0,255), rand(0,255), rand(0,255));
-// const blocks = choose(["L", "square"])
 
 
-//adds starting scene
+
+//adds logo
 add([
   sprite("logo"),
   pos(width()/2-25, height()/2),
@@ -39,90 +43,131 @@ add([
   scale(0.75)
 ])
 
-  add([
-    text("click \n to start game"),
+//adds button to start the game
+function addGameButton(txt, p, f) {
+  const btn = add([
+    text(txt),
+    pos(width()/2,90),
     origin("center"),
-    pos(width()/2, 90),
-    scale(0.5),
+    area({ cursor: "pointer", }),
+    scale(0.4),
     ])
+          
+       btn.onClick(f)
+          
+    }
+         
+    addGameButton("click here\nto start the game", vec2(20, 10), () => go ("game"))
 
-onMouseDown(() => {
-  go("game")
-}),
+//adds game-description
+add([
+  text("GAME DESCRIPTION:\nIn Tetris, players complete lines\nby moving tetrominoes,\nwhich descend onto the playing field.\nThe completed lines disappear and grant the player points,\nand the player can proceed to fill the spaces.\nThe game ends when the uncleared lines\nreach the top of the playing field"),
+  pos(10, 650),
+  scale(0.25),
+])
+
+//adds button for credit scene
+function addButton(txt, p, f) {
+const btn = add([
+  text(txt),
+   pos(1090,750),
+   area({ cursor: "pointer", }),
+   scale(0.4),
+  ])
+    
+      btn.onClick(f)
+    
+    }
+   
+    addButton("click HERE", vec2(20, 10), () => go ("credits"))
+
+add([
+  text("to see the credit scene"),
+  pos(1050,780),
+  scale(0.25),
+])
+
+// to access the credit scene
+scene ("credits", () => {
+  add([
+    text("this game was made by: \n\n\n\n\n Anina de Gennaro \n\n\n Janina Alleman \n\n\n Rahel Hasler \n\n\n Rahel Ackeret \n\n\n\n\n\n\n\n thankyou for playing"),
+    origin ("center"),
+    scale(0.5),
+    pos (width()/2, 1300),
+    body(),
+    area(),
+  ])
+
+gravity(-50);
+
+//adds button to go back to the game
+function addButton(txt, p, f) {
+  const btn = add([
+    text(txt),
+     pos(1200,780),
+     area({ cursor: "pointer", }),
+     scale(0.3),
+    ])
+      
+        btn.onClick(f)
+      
+      }
+     
+      addButton("new game", vec2(20, 10), () => go ("game"))
+
+
+})
 
 
 
-  
 
 // //adds game-scene
   scene("game", () => {
 
-const player = add ([
-  sprite(spriteName),
-  pos(width()/2-25,30),
-  body(),
-  area(),
-  scale(2),
-  spriteColor,
-  "block",
-  {isMoving:true}
-]);
-
-
-// //adds movement
-// keyDown("right", () => {
-//   player.move(MOVE_SPEED, 0)
-// }),
-
-//   keyDown("left", () => {
-//     player.move(-MOVE_SPEED, 0)
-//   }),
-
-//   keyDown("down", () => {
-//     player.move(0, MOVE_SPEED)
-//   });
-
-// //if player hits ground it stops
-// player.collides("ground", () => {
-//   MOVE_SPEED = STAY_SPEED
-//   score.value++
-//   score.text = score.value
-// })
-
-// //if the player connects with the ceiling it will go to the lose scene
-// player.collides("ceiling", () => 
-//   go("lose"),
-//  )
-
-//  //rotates block
-//  onKeyPress("space", () => {
-//   if (player.active) {
-//     player.angle += 90;
-//   }
-// })
-
-//when player hits ground another block appears
-player.collides("ground", () => {
-  createRandomBlock(player.pos.x, player.pos.y);
-});
-
-
 //creates random blocks
-function createRandomBlock (x,y) {
+function createBlock  (x,y) {
   add ([
     sprite(spriteName),
-    pos(width()/2-25,30),
+    pos(width()/2-25,50),
     body(),
     area(),
     scale(2),
+    rotate(),
+    origin("center"),
     spriteColor,
     "block",
     {
       isMoving: true
-    }
-  ]);
+    },
+    {
+      active : true
+    },
+  ])
+  collides("block", "ground", (b) => {
+    b.isMoving = false;
+    score.text = score.value
+    score.value++
+  }) 
+  collides("block", "block", (b) => {
+    b.isMoving = false;
+  })
+  collides("block", "ground", (b) => {
+    b.active = false;
+  })
+  collides("block", "block", (b) => {
+    b.active = false;
+  })
+  collides("block", "ceiling", (b) => 
+    go("lose"),
+  )
 }
 
+//spawns a new block every 5 seconds (TIME_INTERVAL is set to 5)
+loop(TIME_INTERVAL, () => {
+  createBlock();
+})
+
+//moves blocks
 keyDown("right", () => {
   every("block", (b) => {
     if(b.isMoving) b.move(MOVE_SPEED,0)
@@ -140,6 +185,16 @@ keyDown("down", () => {
     if(b.isMoving) b.move(0, MOVE_SPEED)
   })
 })
+
+//rotates blocks
+keyDown("space", () => {
+  every("block", (b) => {
+    if(b.active) {
+      b.angle = 90;
+    } 
+  })
+})
+
 
 
 
@@ -338,20 +393,6 @@ scene("lose", () => {
 
 })
 
-//adds credit scene that moves upwards due to negativ gravity
-scene ("credits", () => {
-  add([
-    text("this game was made by: \n\n\n\n\n Anina de Gennaro \n\n\n Janina Alleman \n\n\n Rahel Hasler \n\n\n Rahel Ackeret \n\n\n\n\n\n\n\n thankyou for playing"),
-    origin ("center"),
-    scale(0.5),
-    pos (width()/2, 1300),
-    body(),
-    area(),
-  ])
-
-gravity(-50);
-
-})
 
 
     })
